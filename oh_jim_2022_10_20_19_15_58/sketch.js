b8c288adaec91fb8a3ed2d7c769972e06821a55a
mazzellaa2 <116307190+mazzellaa2@users.noreply.github.com>
2022-10-20 15:16:47 -0400

    Add files via upload

---
+++ oh_jim_2022_10_20_19_15_58/sketch.js
@@ -0,0 +1,35 @@
+var x, y, s1, s2;
+
+function setup() {
+  createCanvas(1920, 1200);
+  rectMode(CENTER);
+  x = width/2;
+  y = height/2;
+  s1 = width;
+  s2= height;
+
+}
+
+function draw() {
+  background(209, 114, 19, 10);
+
+  frameRate(13);
+  fill(66, 197, 245, 70);
+  noStroke();
+  rect(x, y, s1, s2);
+
+  s1 = s1 - 50;
+  s2 = s2 - 50;
+
+  if (s1 < 960) {
+    s1 = width;
+    s2=height;
+  }
+
+//   if(s2<540){
+//   s2=height;
+//   }
+
+  
+}
+11, 72, 94
